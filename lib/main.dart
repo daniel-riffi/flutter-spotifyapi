26602ead177c312040bf574adb6af348a2ba48d5
daniel-riffi <riffert.daniel@gmail.com>
2020-07-07 16:11:25 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,82 @@
+import 'package:audioplayer/audioplayer.dart';
+import 'package:flutter/material.dart';
+import 'package:spotify/spotify.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  final String clientId = '7605835d7b0c4efd952bb9b511d824dc';
+  final String clientSecret = '57cdae8d450c49f1b8b851f1fb4c31d7';
+
+  AudioPlayer audioPlayer = AudioPlayer();
+
+  void getArtist() async {
+    final credentials = SpotifyApiCredentials(clientId, clientSecret);
+    final spotify = SpotifyApi(credentials);
+    final track = await spotify.tracks.get('437SS64LgwA8NBqMkI0zwP');
+    //audioPlayer.play("https://p.scdn.co/mp3-preview/3eb16018c2a700240e9dfb8817b6f2d041f15eb1?cid=774b29d4f13844c495f206cafdad9c86");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    getArtist();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
